204aca8cff731d00df513dd944eb9e7db60e9eb8
gavin liu <1824306327@163.com>
2026-06-27 21:02:41 +0800

    fix android fstab to linux

--- scripts/11-config-fstab.sh
+++ scripts/11-config-fstab.sh
@@ -5,6 +5,6 @@ echo "[$(date +'%Y-%m-%d %H:%M:%S')] [11] 🗂️ 配置 fstab"
 
 echo "PARTLABEL=userdata / ext4 errors=remount-ro,x-systemd.growfs 0 1
 PARTLABEL=cache /boot vfat umask=0077 0 1
-PARTLABEL=persist /var/lib/rmtfs ext4 nosuid,nodev,noatime  wait,check" > rootdir/etc/fstab
+PARTLABEL=persist /var/lib/rmtfs ext4 nosuid,nodev,noatime 0 2" > rootdir/etc/fstab
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [11] ✅ fstab 配置完成"
